e761f1d642a3e5ac79c4b2e8b709f9047923a64b
lgtm <1gtm@users.noreply.github.com>
2021-08-24 13:07:50 -0700

    Generate code for provider@v1.28.1-0.20210729083022-bc68146ed6f3 gen@7377b18 (#9)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- controllers/cis/certificate_upload_controller.go
+++ controllers/cis/certificate_upload_controller.go
@@ -20,8 +20,8 @@ package cis
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/go-errors/errors"
 	"github.com/go-logr/logr"
 	tfschema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	auditlib "go.bytebuilders.dev/audit/lib"
@@ -118,7 +118,7 @@ func (r *CertificateUploadReconciler) SensitiveSecretWatch(ctx context.Context) 
 
 		sensSec, ok := o.(*v1.Secret)
 		if !ok {
-			log.Error(errors.Errorf("expected a Secret but go a %T", o), "failed to get secret for CertificateUpload")
+			log.Error(fmt.Errorf("expected a Secret but go a %T", o), "failed to get secret for CertificateUpload")
 			return nil
 		}
 
